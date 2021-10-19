ab41fae981272f9403eafa3f6832fe53e0e48500
RoxyRoses <rosatuttie@gmail.com>
2021-10-19 14:41:00 -0300

    Adicionar arquivos de projeto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,216 @@
+﻿
+namespace Supermercados
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.guna2CircleButton1 = new Guna.UI2.WinForms.Guna2CircleButton();
+            this.usuarioTextBox = new Guna.UI2.WinForms.Guna2TextBox();
+            this.loginLabel = new System.Windows.Forms.Label();
+            this.guna2ComboBox1 = new Guna.UI2.WinForms.Guna2ComboBox();
+            this.senhaTextBox = new Guna.UI2.WinForms.Guna2TextBox();
+            this.usuarioLabel = new System.Windows.Forms.Label();
+            this.senhaLabel = new System.Windows.Forms.Label();
+            this.loginButton = new Guna.UI2.WinForms.Guna2Button();
+            this.SuspendLayout();
+            // 
+            // guna2CircleButton1
+            // 
+            this.guna2CircleButton1.CheckedState.Parent = this.guna2CircleButton1;
+            this.guna2CircleButton1.CustomImages.Parent = this.guna2CircleButton1;
+            this.guna2CircleButton1.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.guna2CircleButton1.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.guna2CircleButton1.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.guna2CircleButton1.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.guna2CircleButton1.DisabledState.Parent = this.guna2CircleButton1;
+            this.guna2CircleButton1.FillColor = System.Drawing.Color.DeepSkyBlue;
+            this.guna2CircleButton1.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.guna2CircleButton1.ForeColor = System.Drawing.Color.White;
+            this.guna2CircleButton1.HoverState.Parent = this.guna2CircleButton1;
+            this.guna2CircleButton1.Location = new System.Drawing.Point(-247, -6);
+            this.guna2CircleButton1.Name = "guna2CircleButton1";
+            this.guna2CircleButton1.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle;
+            this.guna2CircleButton1.ShadowDecoration.Parent = this.guna2CircleButton1;
+            this.guna2CircleButton1.Size = new System.Drawing.Size(513, 480);
+            this.guna2CircleButton1.TabIndex = 0;
+            // 
+            // usuarioTextBox
+            // 
+            this.usuarioTextBox.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.usuarioTextBox.DefaultText = "";
+            this.usuarioTextBox.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.usuarioTextBox.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.usuarioTextBox.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.usuarioTextBox.DisabledState.Parent = this.usuarioTextBox;
+            this.usuarioTextBox.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.usuarioTextBox.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.usuarioTextBox.FocusedState.Parent = this.usuarioTextBox;
+            this.usuarioTextBox.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.usuarioTextBox.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.usuarioTextBox.HoverState.Parent = this.usuarioTextBox;
+            this.usuarioTextBox.Location = new System.Drawing.Point(466, 195);
+            this.usuarioTextBox.Name = "usuarioTextBox";
+            this.usuarioTextBox.PasswordChar = '\0';
+            this.usuarioTextBox.PlaceholderText = "";
+            this.usuarioTextBox.SelectedText = "";
+            this.usuarioTextBox.ShadowDecoration.Parent = this.usuarioTextBox;
+            this.usuarioTextBox.Size = new System.Drawing.Size(200, 36);
+            this.usuarioTextBox.TabIndex = 1;
+            // 
+            // loginLabel
+            // 
+            this.loginLabel.AutoSize = true;
+            this.loginLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.loginLabel.ForeColor = System.Drawing.Color.DeepSkyBlue;
+            this.loginLabel.Location = new System.Drawing.Point(528, 69);
+            this.loginLabel.Name = "loginLabel";
+            this.loginLabel.Size = new System.Drawing.Size(60, 25);
+            this.loginLabel.TabIndex = 2;
+            this.loginLabel.Text = "Login";
+            this.loginLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // guna2ComboBox1
+            // 
+            this.guna2ComboBox1.BackColor = System.Drawing.Color.Transparent;
+            this.guna2ComboBox1.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
+            this.guna2ComboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.guna2ComboBox1.FocusedColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.guna2ComboBox1.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.guna2ComboBox1.FocusedState.Parent = this.guna2ComboBox1;
+            this.guna2ComboBox1.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.guna2ComboBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(68)))), ((int)(((byte)(88)))), ((int)(((byte)(112)))));
+            this.guna2ComboBox1.HoverState.Parent = this.guna2ComboBox1;
+            this.guna2ComboBox1.ItemHeight = 30;
+            this.guna2ComboBox1.ItemsAppearance.Parent = this.guna2ComboBox1;
+            this.guna2ComboBox1.Location = new System.Drawing.Point(465, 129);
+            this.guna2ComboBox1.Name = "guna2ComboBox1";
+            this.guna2ComboBox1.ShadowDecoration.Parent = this.guna2ComboBox1;
+            this.guna2ComboBox1.Size = new System.Drawing.Size(201, 36);
+            this.guna2ComboBox1.TabIndex = 3;
+            // 
+            // senhaTextBox
+            // 
+            this.senhaTextBox.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.senhaTextBox.DefaultText = "";
+            this.senhaTextBox.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.senhaTextBox.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.senhaTextBox.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.senhaTextBox.DisabledState.Parent = this.senhaTextBox;
+            this.senhaTextBox.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.senhaTextBox.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.senhaTextBox.FocusedState.Parent = this.senhaTextBox;
+            this.senhaTextBox.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.senhaTextBox.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.senhaTextBox.HoverState.Parent = this.senhaTextBox;
+            this.senhaTextBox.Location = new System.Drawing.Point(466, 256);
+            this.senhaTextBox.Name = "senhaTextBox";
+            this.senhaTextBox.PasswordChar = '\0';
+            this.senhaTextBox.PlaceholderText = "";
+            this.senhaTextBox.SelectedText = "";
+            this.senhaTextBox.ShadowDecoration.Parent = this.senhaTextBox;
+            this.senhaTextBox.Size = new System.Drawing.Size(200, 36);
+            this.senhaTextBox.TabIndex = 4;
+            // 
+            // usuarioLabel
+            // 
+            this.usuarioLabel.AutoSize = true;
+            this.usuarioLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.usuarioLabel.ForeColor = System.Drawing.Color.DeepSkyBlue;
+            this.usuarioLabel.Location = new System.Drawing.Point(359, 206);
+            this.usuarioLabel.Name = "usuarioLabel";
+            this.usuarioLabel.Size = new System.Drawing.Size(79, 25);
+            this.usuarioLabel.TabIndex = 5;
+            this.usuarioLabel.Text = "Usuario";
+            this.usuarioLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // senhaLabel
+            // 
+            this.senhaLabel.AutoSize = true;
+            this.senhaLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.senhaLabel.ForeColor = System.Drawing.Color.DeepSkyBlue;
+            this.senhaLabel.Location = new System.Drawing.Point(359, 267);
+            this.senhaLabel.Name = "senhaLabel";
+            this.senhaLabel.Size = new System.Drawing.Size(70, 25);
+            this.senhaLabel.TabIndex = 6;
+            this.senhaLabel.Text = "Senha";
+            this.senhaLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // loginButton
+            // 
+            this.loginButton.CheckedState.Parent = this.loginButton;
+            this.loginButton.CustomImages.Parent = this.loginButton;
+            this.loginButton.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.loginButton.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.loginButton.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.loginButton.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.loginButton.DisabledState.Parent = this.loginButton;
+            this.loginButton.FillColor = System.Drawing.Color.DeepSkyBlue;
+            this.loginButton.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.loginButton.ForeColor = System.Drawing.Color.White;
+            this.loginButton.HoverState.Parent = this.loginButton;
+            this.loginButton.Location = new System.Drawing.Point(477, 343);
+            this.loginButton.Name = "loginButton";
+            this.loginButton.ShadowDecoration.Parent = this.loginButton;
+            this.loginButton.Size = new System.Drawing.Size(180, 45);
+            this.loginButton.TabIndex = 7;
+            this.loginButton.Text = "Login";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(698, 450);
+            this.Controls.Add(this.loginButton);
+            this.Controls.Add(this.senhaLabel);
+            this.Controls.Add(this.usuarioLabel);
+            this.Controls.Add(this.senhaTextBox);
+            this.Controls.Add(this.guna2ComboBox1);
+            this.Controls.Add(this.loginLabel);
+            this.Controls.Add(this.usuarioTextBox);
+            this.Controls.Add(this.guna2CircleButton1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2CircleButton guna2CircleButton1;
+        private Guna.UI2.WinForms.Guna2TextBox usuarioTextBox;
+        private System.Windows.Forms.Label loginLabel;
+        private Guna.UI2.WinForms.Guna2ComboBox guna2ComboBox1;
+        private Guna.UI2.WinForms.Guna2TextBox senhaTextBox;
+        private System.Windows.Forms.Label usuarioLabel;
+        private System.Windows.Forms.Label senhaLabel;
+        private Guna.UI2.WinForms.Guna2Button loginButton;
+    }
+}
+
